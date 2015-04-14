72e582131f0af86e829962c135a8f5bc57a68a8d
Mike Hockerman <mike@hockerman.com>
2015-04-13 22:45:24 -0400

    Fixed bug with whitespace generation in runner. Updated logans test cases to his april 1 release.

--- main.py
+++ main.py
@@ -28,6 +28,35 @@ def stripEndNl(st):
         return stripEndNl(st[:len(st)-1])
     return st
 
+def getTerminalSize():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        ### Use get(key[, default]) instead of a try/catch
+        #try:
+        #    cr = (env['LINES'], env['COLUMNS'])
+        #except:
+        #    cr = (25, 80)
+    return int(cr[1]), int(cr[0])
+
 # ==================
 # Printing Functions
 # ==================
@@ -49,8 +78,9 @@ def blue(st):
     sys.stdout.flush()
 
 def clearLine():
+    (width, height) = getTerminalSize()
     sys.stdout.write('\r')
-    for i in xrange(0, 100):
+    for i in xrange(0, width):
         sys.stdout.write(' ')
     sys.stdout.write('\r')
     sys.stdout.flush()
